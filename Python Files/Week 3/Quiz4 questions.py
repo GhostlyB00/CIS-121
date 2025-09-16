f82462ce70d7489f14f8495f6106e5e0a31f92bc
Seth Johnson <Sethjohnson0511@gmail.com>
2025-09-16 09:45:52 -0500

    Week 4
    Working on loops this week and he is my code

---
+++ Python Files/Week 3/Quiz4 questions.py
@@ -0,0 +1,6 @@
+if my_var % 2 ==1 :
+    if my_var ** 3 != 27:
+        my_var =my_var + 4
+    else:
+        my_var /= 1.5
+        
---
+++ Python Files/Week 4/loops.py
@@ -0,0 +1,10 @@
+#number = 2
+#while number <= 10:
+    #print(number)
+    #number +=2
+
+number = 5
+while number <= 10:
+    if number %2==1:
+        print(number)
+    number +=1
