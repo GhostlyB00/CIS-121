f632f51fe2ae9aa1d53cb3df581d52712dd4c801
Seth Johnson <Sethjohnson0511@gmail.com>
2025-09-25 11:46:48 -0500

    in class work

---
+++ Python Files/Week 5/lists.py
@@ -0,0 +1,20 @@
+#in python, a list starts with a [ and end with ]
+
+x[] #this is a list with nothing in it .
+
+lyst = ['apple' , 'banana', 3 , False , 4.5, 'grapes'] 
+
+# print the element of the lyst in position 1.
+print(lyst,[1])
+
+#print everything that is not a string
+
+print(lyst[2:6])
+
+x = 'apple'
+print(x)
+
+#lyst.append( element ) will add the element to the end of the lyst.
+
+lyst.append(12)
+print(lyst)
