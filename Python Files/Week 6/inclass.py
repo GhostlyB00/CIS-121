769a5a5343b8640cbee861bf5536d9fccdc4de9a
Seth Johnson <Sethjohnson0511@gmail.com>
2025-09-30 09:33:23 -0500

    Worked on in class showing how to change strings in range

---
+++ Python Files/Week 6/inclass.py
@@ -0,0 +1,42 @@
+lyst = ['Monday', 'tuesday', 'wednesday', 'thursday', 'friday']
+
+#print (x[2])
+#print(x[1:4])
+#for element in lyst:
+#   print(element)
+
+
+'''
+print(lyst)
+lyst.append('saturday')
+lyst.append('sunday')
+print(lyst)
+
+
+word = 'apple'
+word += 'e'
+print(word)
+
+
+print(lyst[2][3:6])
+
+
+
+x = 'Wednesday'
+print(x[3:6])
+
+
+print(lyst[4])
+
+lyst[4] = 'Funday'
+
+print(lyst)
+
+'''
+
+word = 'apfel'
+print(word)
+
+
+word[2] = 'p'
+print(word[2])
